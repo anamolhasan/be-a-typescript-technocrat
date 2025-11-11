a9277b021e0b60f3854ebcfbf02e29724a61083a
anamolhasan <mdanamolhasan1998@gmail.com>
2025-11-11 11:11:38 +0600

    primitiv type

---
+++ src/primitive.ts
@@ -0,0 +1,14 @@
+// JS - TS
+// string, number, undefined, null
+
+// TS: never, unknown, void
+
+let userName:string = 'anam'
+let userId : number = 13
+// userName.toFixed()
+userId.toFixed()
+
+let isAdmin: boolean = false
+isAdmin = true
+
+let x: undefined = undefined
