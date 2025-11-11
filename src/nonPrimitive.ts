b32d8a160f099a71754a97a6a2f9e007a883baea
anamolhasan <mdanamolhasan1998@gmail.com>
2025-11-11 12:56:42 +0600

    nonpromitive learing

---
+++ src/nonPrimitive.ts
@@ -0,0 +1,54 @@
+// array object
+
+// ts - tuple
+
+let bazarList : string[] = ['eggs', 'milk', 'sugar']
+
+// bazarList.push(true)
+
+let mixedArr: (string | number)[] = ['eggs',12, 'milk', 1, 'sugar', 2]
+
+// mixedArr.push(true)
+
+let coordinates:[number, number] = [20, 30, 50]
+
+let couple: [string, string] = ['Husband', 'wife']
+
+let destination: [string, string, number] = ['Dhaka', 'Chattogram', 3]
+
+
+// reference type : object
+
+// const user : {
+//     organization: "Programming Hero";  // value => type : literal types
+//     firstName: string;
+//     middleName: string;
+//     lastName:string;
+//     isMarried:boolean;
+// } = {
+//     organization:"Programming Hero",
+//     firstName:'Jhankar',
+//     lastName:'Mahbub',
+//     isMarried:true
+// }
+//  user.organization = 'Programming Hero'
+
+
+const user : {
+    organization: "Programming Hero";  // access modifier
+    firstName: string;
+    middleName?: string; // optional type
+    lastName:string;
+    isMarried:boolean;
+} = {
+    organization:"Programming Hero",
+    firstName:'Jhankar',
+    lastName:'Mahbub',
+    isMarried:true
+}
+
+user.organization = 'Programming Hero'
+
+console.log(user)
+
+console.log(user)
